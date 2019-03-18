da9064afc13eaf334dd57c9432270de084d278d6
svodnik <sashavodnik@gmail.com>
2019-03-18 13:45:10 -0700

    added resources for Class 9

---
+++ 09-ajax-apis/solution-code/7-ajax-lab/js/keys.js
@@ -0,0 +1,8 @@
+// API keys
+
+// openweathermap.org
+const apiKey = 'd126cacbbfebf7c84ad878e9deffc0e1';
+
+// weatherunlocked.com
+const appID2 = 'ddbdb45a';
+const apiKey2 = 'c2b55cdde0f9069dae3ba1d761748284';
